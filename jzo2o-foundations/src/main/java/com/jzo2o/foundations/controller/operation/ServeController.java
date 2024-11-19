189f304c9ab318b2ec2c12c1e883bd1b63e97d72
branchtree <branchtree97@outlook.com>
2024-11-19 13:14:24 +0800

    Initial commit

---
+++ jzo2o-foundations/src/main/java/com/jzo2o/foundations/controller/operation/ServeController.java
@@ -0,0 +1,16 @@
+package com.jzo2o.foundations.controller.operation;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 区域服务管理相关接口
+ * </p>
+ *
+ * @author branchtree
+ * @since 2024-11-19
+ */
+@RestController("operationServeController")
+public class ServeController {
+}
